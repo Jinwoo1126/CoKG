6f231ef937e4c3ee97c19946379a8c97032b5404
장진우 <jinwoo@jangjin-uui-MacStudio.local>
2024-08-04 17:13:53 +0900

    (feat:4) geval update

--- evaluate.py
+++ evaluate.py
@@ -8,19 +8,7 @@ from src.eval import rouge, GEval
 from src.utils import print_scores
 
 
-if __name__ == "__main__":
-    argparser = ArgumentParser()
-    argparser.add_argument("-m", "--model", type=str, default='gpt-4o-mini', help="Model to use")
-    argparser.add_argument("-r", "--results", type=str, default='results/results.json', help="Results file")
-    argparser.add_argument("-s", "--save_fp", type=str, default='results/')
-    args = argparser.parse_args()
-
-    with open(args.results, 'r') as f:
-        results = json.load(f)
-
-    references = [r['Document'] for r in results]
-    hypotheses = [r['Summary'] for r in results]
-
+def evaluate_rouge(hypotheses, references):
     rouge_score = rouge(hypotheses, references)
 
     print("ROUGE Scores")
@@ -33,9 +21,8 @@ if __name__ == "__main__":
     print_scores(rouge_score['rougeL'])
     print("============")
 
-    
-    ## GEval
-    ## Load .env and OpenAI API key
+
+def evaluate_geval(args, hypotheses, references):
     load_dotenv()
     openai_key = os.getenv("OPENAI_API")
 
@@ -51,3 +38,31 @@ if __name__ == "__main__":
     print("GEval")
     print_scores(geval_score)
     print("============")
+
+
+if __name__ == "__main__":
+    argparser = ArgumentParser("Evaluate text with various metrics.")
+    argparser.add_argument("-t", "--type", choices=['rouge', 'geval', 'all'], required=True, help="Choose evaluation metrics: 'rouge', 'geval', or 'both'.")
+    argparser.add_argument("-m", "--model", type=str, default='gpt-4o-mini', help="Model to use")
+    argparser.add_argument("-r", "--results", type=str, default='results/results.json', help="Results file")
+    argparser.add_argument("-s", "--save_fp", type=str, default='results/')
+    args = argparser.parse_args()
+
+    with open(args.results, 'r') as f:
+        results = json.load(f)
+
+    references = [r['Document'] for r in results]
+    hypotheses = [r['Summary'] for r in results]
+
+    if args.type == 'rouge':
+        evaluate_rouge(hypotheses, references)
+    elif args.type == 'geval':
+        evaluate_geval(args, hypotheses, references)
+    elif args.type == 'all':
+        evaluate_rouge(hypotheses, references)
+        evaluate_geval(args, hypotheses, references)
+    else:
+        evaluate_rouge(hypotheses, references)
+        evaluate_geval(args, hypotheses, references)
+
+    
