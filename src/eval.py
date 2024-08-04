3aec60e5f3aca4c76dbcc53061034399828e22cf
장진우 <jinwoo@jangjin-uui-MacStudio.local>
2024-08-04 17:07:04 +0900

    (feat:3) geval update

--- src/eval.py
+++ src/eval.py
@@ -1,5 +1,29 @@
+import os
+import time
+import json
+import copy
+import re
+
 from tqdm import tqdm
+from openai import OpenAI
 from rouge_score import rouge_scorer
+from scipy.stats import spearmanr, pearsonr, kendalltau
+
+
+current_file_path = os.path.abspath(__file__)
+current_dir = os.path.dirname(current_file_path)
+
+
+def parse_output(output):
+    matched = re.search(r'"[^"]+": (\d+)', output)
+    if (matched):
+        try:
+            score = float(matched.group(1))
+        except:
+            score = 0
+    else:
+        score = 0
+    return score
 
 
 def rouge(hypotheses, references):
@@ -24,3 +48,61 @@ def rouge(hypotheses, references):
         score[key]['fmeasure'] /= len(hypotheses)
     
     return score
+
+
+class GEval:
+    def __init__(self, args, api_key, hypotheses, references):
+        self.args = args
+        self.api_key = api_key
+        self.hypotheses = hypotheses
+        self.references = references
+        self.metrics = {
+            'coherence': open(os.path.join(current_dir, 'geval', 'coh_detailed.txt')).read(),
+            'consistency': open(os.path.join(current_dir, 'geval', 'con_detailed.txt')).read(),
+            'fluency': open(os.path.join(current_dir, 'geval', 'flu_detailed.txt')).read(),
+            'relevance': open(os.path.join(current_dir, 'geval', 'rel_detailed.txt')).read()
+        }
+
+    def run(self):
+        for metric in self.metrics.keys():
+            for i, (hyp, ref) in tqdm(enumerate(zip(self.hypotheses, self.references))):    
+                prompt = copy.deepcopy(self.metrics[metric])
+                cur_prompt = prompt.replace('{{Document}}', ref).replace('{{Summary}}', hyp)
+            
+                client = OpenAI(api_key=self.api_key)
+                _response = client.chat.completions.create(
+                    model=self.args.model,
+                    response_format={ "type": "json_object" },
+                    messages=[{"role": "system", "content": cur_prompt}],
+                    temperature=2,
+                    max_tokens=20,
+                    top_p=1,
+                    frequency_penalty=0,
+                    presence_penalty=0,
+                    stop=None,
+                    n=20
+                )
+                time.sleep(0.5)
+
+                all_responses = [_response.choices[i].message.content for i in
+                                    range(len(_response.choices))]
+                new_json = {
+                    'Document': ref,
+                    'Summary': hyp,
+                    'Responses': all_responses
+                }
+
+            os.makedirs(self.args.save_fp, exist_ok=True)
+            with open(os.path.join(self.args.save_fp, metric + '.json'), 'w') as f:
+                json.dump(new_json, f, indent=4)
+
+    def evaluate(self):
+        scores = {}
+        for metric in self.metrics.keys():
+            with open (os.path.join(self.args.save_fp, metric + '.json'), 'r') as f:
+                all_responses = json.load(f)["Responses"]
+                all_scores = [parse_output(x) for x in all_responses]
+                score = sum(all_scores) / len(all_scores)
+                scores[metric] = score
+
+        return scores
