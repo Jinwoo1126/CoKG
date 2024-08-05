bd3a3538c6943c29ddd723ca6c99e4399ebc0f3a
zerojin91 <zerojin0502@gmail.com>
2024-08-05 07:19:45 +0000

    fix&add

---
+++ .ipynb_checkpoints/run-checkpoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+python3 main.py -t 'CoKG' -n 100 -o 'results/CoKG_results.json'
+python3 evaluate.py -t 'rouge' -r 'results/CoKG_results.json'
+python3 main.py -t 'CoE' -n 100 -o 'results/CoE_results.json'
+python3 evaluate.py -t 'rouge' -r 'results/CoE_results.json'
+python3 main.py -t 'CoD' -n 100 -o 'results/CoD_results.json'
+python3 evaluate.py -t 'rouge' -r 'results/CoD_results.json'
+python3 main.py -t 'Base' -n 100 -o 'results/Base_results.json'
+python3 evaluate.py -t 'rouge' -r 'results/Base_results.json'
